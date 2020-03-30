63687a41d3476cd915e9a58e788baefb259b0c4f
nagendra <=>
2020-03-30 14:01:38 +0530

    added fibonacci function to loops.c

--- loops.c
+++ loops.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 long int factorial(int);
+void fibonacci(int);
 
 int main(void){
   int number;
@@ -8,9 +9,27 @@ int main(void){
   printf("enter a number to calculate factorial :");
   scanf("%d",&number);
   printf("Factorial of %d is %ld\n",number,factorial(number));
+
+  printf("enter a number ");
+  scanf("%d",&number);
+  fibonacci(number);
   return 0;
 }
 
 long int factorial(int number){
 return number==0?1:number*factorial(number-1);
 }
+
+void fibonacci(int number){
+  printf("Fibonacci series under %d is :\n",number);
+  int previousNumber=0;
+  int currentNumber =1;
+  printf("%d\n",previousNumber);
+  while(currentNumber<number){
+   int temp = currentNumber;
+   printf("%d\n",currentNumber);
+   currentNumber=previousNumber+currentNumber;
+   previousNumber=temp;
+     }
+}
+
