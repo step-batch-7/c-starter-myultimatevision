19279ae5e64b19d2bbf34d039277a186e82793cf
nagendra <=>
2020-03-30 10:46:10 +0530

    added gcd function to basic_functions.c

--- basic_functions.c
+++ basic_functions.c
@@ -4,22 +4,33 @@ int isEven(int);
 int isOdd(int);
 int square(int);
 int cube(int);
+int gcd(int,int);
 
 int main(void)
 {
-  int number;
+  int number1,number2;
+
   printf("enter a number to check even or not : ");
-  scanf("%d",&number);
-  printf("%d is %s number\n",number,isEven(number)?"an even":"not an even");
+  scanf("%d",&number1);
+  printf("%d is %s number\n",number1,isEven(number1)?"an even":"not an even");
+
   printf("enter a number to check odd or not : ");
-  scanf("%d",&number);
-  printf("%d is %s number\n",number,isOdd(number)?"a odd":"not a odd");
+  scanf("%d",&number1);
+  printf("%d is %s number\n",number1,isOdd(number1)?"a odd":"not a odd");
+
   printf("enter a number to find Square : ");
-  scanf("%d",&number);
-  printf("square of %d is %d\n",number,square(number));
+  scanf("%d",&number1);
+  printf("square of %d is %d\n",number1,square(number1));
+
   printf("enter a number to find Cube : ");
-  scanf("%d",&number);
-  printf("cube of %d is %d\n",number,cube(number));
+  scanf("%d",&number1);
+  printf("cube of %d is %d\n",number1,cube(number1));
+
+  printf("enter two numbers to find GCD : ");
+  scanf("%d",&number1);
+  scanf("%d",&number2);
+  printf("GCD of %d and %d is %d\n",number1,number2,gcd(number1,number2));
+
   return 0;
 };
 
@@ -41,5 +52,13 @@ int square(int number)
 int cube(int number)
 {
  return number*square(number);
+}
+
+int gcd(int number1,int number2){
+  if(number1%number2==0)
+  {
+    return number2;
+  }
+  return gcd(number2,number1 % number2);
 }
 
